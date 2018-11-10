af269db7eaf3cb044c9752fc3937f3cac597ce47
Muthu Kumar <muthukumar@thefeathers.in>
2018-11-10 13:15:00 +0530

    [wrapper] Complete with Typings!

--- wrapper/wrapper.ts
+++ wrapper/wrapper.ts
@@ -1,13 +1,36 @@
-import m from 'mithril';
-import htmlElements from '../assets/htmlElements.json';
+import m, { Vnode, Children, Attributes } from 'mithril';
 import elementNames from '../assets/htmlElements';
 
-type Elements = { [name in elementNames]: (...children: string[]) => m.Vnode }
+/* Creates a virtual element (Vnode). */
+interface CurriedHyperScript {
+	(...children: Children[]): Vnode<any, any>;
+	(attributes: Attributes, ...children: Children[]): Vnode<any, any>;
+}
 
-const elems = htmlElements.reduce(
-	(elements: object, el: string): Elements => (
-		{ ...elements, [el]: (...children: string[]) => m(el, ...children)} as Elements
-	), {}
-) as Elements;
+interface MithrilScript extends CurriedHyperScript {
+	[key: string]: MithrilScript
+}
+
+function createMithrilScript (selector: string): MithrilScript
+function createMithrilScript (selector: string) {
+	return (...args: any[]) => m(selector, ...args);
+}
+
+type Elements = { [name in elementNames]: MithrilScript }
+
+const createClassProxy = (h: CurriedHyperScript, name: any): any => new Proxy(h, {
+	get: (f, selector: string) => {
+		const isPureClass =
+			selector.split('.').length === 1
+			&& selector.split('#').length === 1;
+		const newSelector = name + `${isPureClass ? '.' : ''}${selector}`;
+		return createClassProxy(createMithrilScript(newSelector), newSelector);
+	}
+});
+
+const elems = new Proxy({}, {
+	get: (obj, name: string) =>
+		createClassProxy(createMithrilScript(name), name)
+}) as Elements;
 
 export default elems;
