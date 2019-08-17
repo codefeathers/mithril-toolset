c4f48137d7f127d8635a461d36dd129dbda7e82e
Muthu Kumar <muthukumar@thefeathers.in>
2019-08-17 22:27:41 +0530

    [refactor]
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var elements_1 = require("./src/elements");
+var elements_1 = require("./elements");
 exports.elements = elements_1.default;
--- index.ts
+++
@@ -1,1 +0,0 @@
-export { default as elements } from './src/elements';
